1d09c644179c187c29436a18c12b6fa246d728f8
Endorsmoch <A16003872@alumnos.uady.mx>
2023-02-25 21:48:00 -0600

    initial commit

---
+++ app/Http/Controllers/Alumnos.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Alumnos extends Controller
+{
+    function lista(){
+        return [["nombre"=>"Rodrigo Urtecho Quintal", "matricula"=>"16003872"],
+        ["nombre"=>"Arath Torres Montero", "matricula"=>"A16008210"], 
+        ["nombre"=>"Sofia Dolores Montes de Oca Quintana", "matricula"=>"19210045"]];
+    }
+}
