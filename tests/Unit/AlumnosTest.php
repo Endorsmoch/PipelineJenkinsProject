5ae227cfb53be0af69d3e09cd09ca75f1d51eab5
Endorsmoch <A16003872@alumnos.uady.mx>
2023-03-16 11:21:01 -0600

    Comments

--- tests/Unit/AlumnosTest.php
+++ tests/Unit/AlumnosTest.php
@@ -4,13 +4,14 @@ namespace Tests\Unit;
 use Mockery;
 use App\Http\Controllers\Alumnos;
 use Tests\TestCase;
-use Routes\Api;
 
 class AlumnosTest extends TestCase
 {
     public function test_listaIsCalled()
     {
+        //Se crea la instancia del Mock
         $mockAlumnos = $this->getMockBuilder(Alumnos::class)->setMethods(['lista'])->getMock();
+        //Se establece lo esperado de la prueba. Ejecutar una sola vez el método lista.
         $mockAlumnos->expects($this->once())->method('lista');
         
         // $this->get('alumnos');
